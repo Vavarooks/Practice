e414a74e72ff08a8ae8845ef960163793cccb705
Vavarooks <vavarooks123@gmail.com>
2020-09-28 10:13:58 -0700

    work

--- Javascript/Java.js
+++ Javascript/Java.js
@@ -4,6 +4,12 @@ function Home() {
   return (
     <div>
       
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> </div>
+    </div>
+
     </div>
   );
 }
