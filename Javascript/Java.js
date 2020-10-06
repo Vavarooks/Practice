5fbc60fbc4fa39c73b668a5420799f9d08bb7b91
Vavarooks <vavarooks123@gmail.com>
2020-10-06 07:02:47 -0700

    work

--- Javascript/Java.js
+++ Javascript/Java.js
@@ -7,7 +7,11 @@ function Home() {
     <div class="jumbotron jumbotron-fluid">
     <div class="container">
     <h1 class="display-4">Azeroth's Cash Grab</h1>
-    <p class="lead">Got that cash? Make more!</p> </div>
+    <p class="lead">Got that cash? Make more!</p>
+    <h2>
+      Times may be tought, but no longer! Just sell your soul today for anything you desier!
+      </h2> 
+    </div>
     </div>
 
     </div>
